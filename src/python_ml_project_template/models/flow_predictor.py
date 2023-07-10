362edc3fce0afdeeec2c5f73c3201de623c82368
liy1shu <liyishu19@mails.tsinghua.edu.cn>
2023-07-10 18:02:04 -0400

    separate network and training module

--- src/python_ml_project_template/models/flow_predictor.py
+++ src/python_ml_project_template/models/flow_predictor.py
@@ -1,19 +1,59 @@
-from typing import Any
+import typing
+from typing import Dict
 
 import lightning as L
-import torch.nn.functional as F
+import plotly.graph_objects as go
+import rpad.visualize_3d.plots as v3p
+import torch
+import torch_geometric.data as tgd
+from flowbot3d.models.artflownet import artflownet_loss, flow_metrics
+from plotly.subplots import make_subplots
 from torch import optim
 
 
-# TODO: flow predictor
+# Flow predictor
 class FlowPredictorTrainingModule(L.LightningModule):
     def __init__(self, network, training_cfg) -> None:
         super().__init__()
         self.network = network
         self.lr = training_cfg.lr
+        self.mask_input_channel = training_cfg.mask_input_channel
 
-    def forward(self, x):
-        self.network(x)
+    def forward(self, data) -> torch.Tensor:  # type: ignore
+        # Maybe add the mask as an input to the network.
+        if self.mask_input_channel:
+            data.x = data.mask.reshape(len(data.mask), 1)
+
+        # Run the model.
+        flow = typing.cast(torch.Tensor, self.network(data))
+
+        return flow
+
+    def _step(self, batch: tgd.Batch, mode):
+        # Make a prediction.
+        f_pred = self(batch)
+
+        # Compute the loss.
+        n_nodes = torch.as_tensor([d.num_nodes for d in batch.to_data_list()]).to(self.device)  # type: ignore
+        f_ix = batch.mask.bool()
+        f_target = batch.flow
+        loss = artflownet_loss(f_pred, f_target, n_nodes)
+
+        # Compute some metrics on flow-only regions.
+        rmse, cos_dist, mag_error = flow_metrics(f_pred[f_ix], f_target[f_ix])
+
+        self.log_dict(
+            {
+                f"{mode}/loss": loss,
+                f"{mode}/rmse": rmse,
+                f"{mode}/cosine_similarity": cos_dist,
+                f"{mode}/mag_error": mag_error,
+            },
+            add_dataloader_idx=False,
+            batch_size=len(batch),
+        )
+
+        return loss
 
     def configure_optimizers(self):
         optimizer = optim.AdamW(self.parameters(), lr=self.lr)
@@ -22,42 +62,112 @@ class FlowPredictorTrainingModule(L.LightningModule):
         )
         return [optimizer], [lr_scheduler]
 
-    def _calculate_loss(self, batch, mode="train"):
-        imgs, labels = batch
-        preds = self.network(imgs)
-        loss = F.cross_entropy(preds, labels)
-        acc = (preds.argmax(dim=-1) == labels).float().mean()
+    def training_step(self, batch: tgd.Batch, batch_id):  # type: ignore
+        self.train()
+        return self._step(batch, "train")
 
-        istrain = mode == "train"
-        self.log("%s_loss" % mode, loss, prog_bar=istrain, add_dataloader_idx=False)
-        self.log("%s_acc" % mode, acc, add_dataloader_idx=False)
-        return {"loss": loss, "acc": acc, "preds": preds}
+    def validation_step(self, batch: tgd.Batch, batch_id, dataloader_idx=0):  # type: ignore
+        self.eval()
+        name = "val" if dataloader_idx == 0 else "unseen"
+        return self._step(batch, name)
 
-    def training_step(self, batch, batch_idx):
-        loss = self._calculate_loss(batch, mode="train")
-        return loss
+    @staticmethod
+    def make_plots(preds, batch: tgd.Batch) -> Dict[str, go.Figure]:
+        obj_id = batch.id
+        pos = batch.pos.numpy()
+        mask = batch.mask.numpy()
+        f_target = batch.flow
+        f_pred = preds
 
-    def validation_step(self, batch, batch_idx, dataloader_idx=0):
-        if dataloader_idx == 0:
-            mode = "train_val"
-        else:
-            mode = "val"
-        print(batch)
-        return self._calculate_loss(batch, mode=mode)
+        fig = make_subplots(
+            rows=2,
+            cols=2,
+            specs=[
+                [{"type": "scene", "colspan": 2}, None],
+                [{"type": "scene"}, {"type": "scene"}],
+            ],
+            subplot_titles=(
+                "input data",
+                "target flow",
+                "pred flow",
+            ),
+            vertical_spacing=0.05,
+        )
 
-    def test_step(self, batch, batch_idx):
-        return self._calculate_loss(batch, mode="test")
+        # Parent/child plot.
+        labelmap = {0: "unselected", 1: "part"}
+        labels = torch.zeros(len(pos)).int()
+        labels[mask == 1.0] = 1
+        fig.add_traces(v3p._segmentation_traces(pos, labels, labelmap, "scene1"))
+
+        fig.update_layout(
+            scene1=v3p._3d_scene(pos),
+            showlegend=True,
+            margin=dict(l=0, r=0, b=0, t=40),
+            legend=dict(x=1.0, y=0.75),
+        )
+
+        # normalize the flow for visualization.
+        n_f_gt = (f_target / f_target.norm(dim=1).max()).numpy()
+        n_f_pred = (f_pred / f_target.norm(dim=1).max()).numpy()
+
+        # GT flow.
+        fig.add_trace(v3p.pointcloud(pos, 1, scene="scene2", name="pts"), row=2, col=1)
+        f_gt_traces = v3p._flow_traces(
+            pos, n_f_gt, scene="scene2", name="f_gt", legendgroup="1"
+        )
+        fig.add_traces(f_gt_traces, rows=2, cols=1)
+        fig.update_layout(scene2=v3p._3d_scene(pos))
+
+        # Predicted flow.
+        fig.add_trace(v3p.pointcloud(pos, 1, scene="scene3", name="pts"), row=2, col=2)
+        f_pred_traces = v3p._flow_traces(
+            pos, n_f_pred, scene="scene3", name="f_pred", legendgroup="2"
+        )
+        fig.add_traces(f_pred_traces, rows=2, cols=2)
+        fig.update_layout(scene3=v3p._3d_scene(pos))
+
+        fig.update_layout(title=f"Object {obj_id}")
+
+        return {"artflownet_plot": fig}
 
 
 class FlowPredictorInferenceModule(L.LightningModule):
-    def __init__(self, network) -> None:
+    def __init__(self, network, inference_config) -> None:
         super().__init__()
         self.network = network
+        self.mask_input_channel = inference_config.mask_input_channel
 
-    def forward(self, x):
-        self.network(x)
+    def forward(self, data) -> torch.Tensor:  # type: ignore
+        # Maybe add the mask as an input to the network.
+        if self.mask_input_channel:
+            data.x = data.mask.reshape(len(data.mask), 1)
 
-    def predict_step(self, batch: Any, batch_idx: int, dataloader_idx: int = 0) -> Any:
-        imgs, labels = batch
-        preds = self.network(imgs)
-        return {"preds": preds, "labels": labels}
+        # Run the model.
+        flow = typing.cast(torch.Tensor, self.network(data))
+
+        return flow
+
+    # TODO: the predict step input is different now, pay attention
+    def predict_step(self, xyz: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
+        """Predict the flow for a single object. The point cloud should
+        come straight from the maniskill processed observation function.
+
+        Args:
+            xyz (torch.Tensor): Nx3 pointcloud
+            mask (torch.Tensor): Nx1 mask of the part that will move.
+
+        Returns:
+            torch.Tensor: Nx3 dense flow prediction
+        """
+        assert len(xyz) == len(mask)
+        assert len(xyz.shape) == 2
+        assert len(mask.shape) == 1
+
+        data = Data(pos=xyz, mask=mask)
+        batch = Batch.from_data_list([data])
+        batch = batch.to(self.device)
+        self.eval()
+        with torch.no_grad():
+            flow = self.forward(batch)
+        return flow
