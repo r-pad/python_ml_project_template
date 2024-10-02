33cbbd1e9f455831e661506ecf0065de77344e01
Ben Eisner <ben.a.eisner@gmail.com>
2024-10-02 18:18:34 -0400

    set up pushing sif files to seuss

---
+++ cluster/build_push_sif_seuss.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Build a docker image, convert it to as singularity image, and push it to the seuss cluster.
+# Right now, this is a total hack since we can't actually build the docker image on the cluster,
+# nor can we build the singularity image on the cluster. So we build the docker image locally,
+# convert it to a singularity image locally, and then push it to the cluster.
+
+# Whole script fails if any command fails.
+
+set -e
+
+# Set some variables.
+dockerhub_username=beisner
+project_name=python_ml_project_template
+scs_username=baeisner
+
+# Get paths.
+script_path=$(realpath $0)
+script_dir=$(dirname $script_path)
+root_dir=$(realpath ${script_dir}/..)
+
+# Compute a good tag for the image, which will be <dockerhub_username>/<project_name>:<branch-name>-scratch.
+sanitized_branch_name=`${script_dir}/sanitize_branch_name.bash`
+
+# Build the docker image.
+docker build -t ${dockerhub_username}/${project_name}:${sanitized_branch_name}-scratch .
+
+# Convert the docker image to a singularity image, and save it in the .singularity_images directory.
+mkdir -p ${root_dir}/.singularity_images
+sif_name=${root_dir}/.singularity_images/${project_name}_${sanitized_branch_name}-scratch.sif
+singularity build ${sif_name} docker-daemon://$dockerhub_username}/${project_name}:${sanitized_branch_name}-scratch
+
+# Rsync the singularity image to the seuss cluster.
+rsync -avz --progress ${sif_name} ${scs_username}@seuss.ri.cmu.edu:/home/${scs_username}/singularity_images/
